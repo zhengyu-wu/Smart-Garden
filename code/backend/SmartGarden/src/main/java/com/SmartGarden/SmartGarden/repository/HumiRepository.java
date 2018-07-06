45c3fd7225c6a5c7fd68eb97f21252ab6595c990
NutterLee <Handong-Lee@sjtu.edu.cn>
2018-07-06 09:09:15 +0800

    完成后端接口基本接口的实现

--- code/backend/SmartGarden/src/main/java/com/SmartGarden/SmartGarden/repository/HumiRepository.java
+++ code/backend/SmartGarden/src/main/java/com/SmartGarden/SmartGarden/repository/HumiRepository.java
@@ -4,6 +4,10 @@ import com.SmartGarden.SmartGarden.model.HumiData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface HumiRepository extends JpaRepository<HumiData,Integer> {
+    List<HumiData> findTop20BySensor_SensorIdOrderBySendTimeDesc(int sensorId);
+    HumiData findTopBySensor_SensorIdOrderBySendTimeDesc(int sensorId);
 }
--- code/backend/SmartGarden/src/main/java/com/SmartGarden/SmartGarden/service/impl/HumiServiceImpl.java
+++ code/backend/SmartGarden/src/main/java/com/SmartGarden/SmartGarden/service/impl/HumiServiceImpl.java
@@ -1,4 +1,68 @@
 package com.SmartGarden.SmartGarden.service.impl;
 
-public class HumiServiceImpl {
+
+import com.SmartGarden.SmartGarden.model.HumiData;
+import com.SmartGarden.SmartGarden.model.Sensor;
+import com.SmartGarden.SmartGarden.model.TempData;
+import com.SmartGarden.SmartGarden.repository.HumiRepository;
+import com.SmartGarden.SmartGarden.service.HumiService;
+import com.SmartGarden.SmartGarden.service.SensorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class HumiServiceImpl implements HumiService {
+
+    @Autowired
+    private HumiRepository humiRepository;
+
+    @Autowired
+    private SensorService sensorService;
+
+
+    @Override
+    public boolean addHumiData(HumiData humiData) {
+        try{
+            humiRepository.save(humiData);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    @Override
+    public boolean deleteByHumiDataId(Integer humiDataId) {
+        try {
+            humiRepository.deleteById(humiDataId);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    @Override
+    public List<HumiData> getLast20DataBySensorId(int sensorId) {
+        return humiRepository.findTop20BySensor_SensorIdOrderBySendTimeDesc(sensorId);
+    }
+
+    @Override
+    public List<HumiData> getLastHumiDataByGardenId(int gardenId) {
+        List<Sensor> tmpSensorList=sensorService.getByGardenId(gardenId);
+        List<HumiData> tmpList=new ArrayList<>();
+        if(tmpSensorList==null)
+            return null;
+        else {
+            for(Sensor tmpSensor:tmpSensorList){
+                HumiData tmpData=null;
+                tmpData=humiRepository.findTopBySensor_SensorIdOrderBySendTimeDesc(tmpSensor.getSensorId());
+                if(tmpData!=null)
+                    tmpList.add(tmpData);
+            }
+        }
+
+        return tmpList;
+    }
 }
