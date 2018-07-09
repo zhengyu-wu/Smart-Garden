d0de31b8536e4566eab54c0e64b60739e9fba0ec
程浩 <weastonCH@sjtu.edu.cn>
2018-07-09 16:38:14 +0800

    modified with localStorage and yet still not finished

---
+++ code/login&register/src/actions/loginActions.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+import setAuthorizationToken from '../utils/setAuthorizationToken'
+
+export const login = (data) => {
+    return dispatch => {
+        return axios.post('')
+    }
+}
