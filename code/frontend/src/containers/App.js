7018c2e0f9a3cdf6b01e1d8c5926baca689ddfff
princehaohao <1906974588@qq.com>
2018-07-09 17:32:33 +0800

    add router

--- code/frontend/src/containers/App.js
+++ code/frontend/src/containers/App.js
@@ -1,13 +1,22 @@
 import React, { Component,PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { BrowserRouter as Router,Route } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import WrappedLoginPage from '../components/LoginPage/LoginPage'
 import WrappedRegisterPage from '../components/RegisterPage/RegisterPage'
+import createHashHistory from 'history/createBrowserHistory'
 import { Grid, Row } from 'react-bootstrap';
 import { Field, reduxForm } from 'redux-form'
 import { auth } from '../actions'
 
+import RouterEntrance from '../components/MenuPage/Menu'
+import { Layout,Icon } from 'antd';
+
+const { Header,Content,Footer,Sider } = Layout;
+
 class App extends Component {
 
+    
     handleSubmit = (values) => {
 
         const { state } = this.props
@@ -20,10 +29,34 @@ class App extends Component {
     }
 
     render() {
+        var hashHistory = createHashHistory();
         const { validateAuth } = this.props.state
 
         return (
-            <WrappedLoginPage val = {validateAuth} onSubmit={this.handleSubmit} />
+            <div>
+                <Header style={{ background: '#000', padding: 0 }}>
+                <span style={{color:'#fff', paddingLeft:'2%', fontSize:'1.4em'}}>
+                    <Icon
+                    className="trigger"
+                    style={{cursor: 'pointer'}}
+                    />
+                    </span>
+                    <span style={{color:'#fff', paddingLeft:'2%', fontSize:'1.4em'}}>Smart Garden</span>
+                </Header>
+                <Layout>
+                    <RouterEntrance/>
+                    {/*
+                    <Sider>
+                        
+                    </Sider>
+                    <Content>
+                        <WrappedLoginPage val = {validateAuth} onSubmit={this.handleSubmit} />
+                    </Content>
+                    */}
+                </Layout>
+            
+            
+            </div>
         );
     }
 }
