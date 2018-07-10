9c0cce526fbea85fd05c1229d092080265471690
NutterLee <Handong-Lee@sjtu.edu.cn>
2018-07-10 14:47:08 +0800

    完成登录登出页面逻辑

--- code/ReactNative/demoWithRedux/src/reducers/user.js
+++ code/ReactNative/demoWithRedux/src/reducers/user.js
@@ -1,18 +1,26 @@
-import { LOAD_USER_FULFILLED, LOAD_USER_PENDING, LOAD_USER_REJECTED } from '../constants';
+import {LOAD_USER_FULFILLED, LOAD_USER_PENDING, LOAD_USER_REJECTED, USER_LOG_OUT} from '../constants';
 import {Toast} from 'antd-mobile-rn';
+import {initialState} from '../state';
 
-const initialState={
-    isFetching: false,
-    error:null,
-    user:{}
-};
 
 const user = (state = initialState, action={}) => {
     switch (action.type) {
         case LOAD_USER_FULFILLED:
+            if(typeof (action.payload.data.userId)==="undefined")
+            {
+                Toast.fail("login fails",1);
+                return{
+                    isFetching: false,
+                    error: "email and password don't match",
+                    hasLogin:false,
+                    user: {}
+                };
+            }
+
             Toast.success("login successfully",1);
             return {
                 isFetching:false,
+                hasLogin:true,
                 error:null,
                 user:action.payload.data
             };
@@ -21,6 +29,7 @@ const user = (state = initialState, action={}) => {
             return {
                 isFetching: true,
                 error: null,
+                hasLogin:false,
                 user: {}
             };
         case LOAD_USER_REJECTED:
@@ -28,6 +37,15 @@ const user = (state = initialState, action={}) => {
             return {
                 isFetching: false,
                 error: action.payload.response.data,
+                hasLogin:false,
+                user: {}
+            };
+        case USER_LOG_OUT:
+            Toast.info("logout successfully",1);
+            return{
+                isFetching: false,
+                error: null,
+                hasLogin:false,
                 user: {}
             };
         default:
---
+++ code/ReactNative/demoWithRedux/src/state/index.js
@@ -0,0 +1,7 @@
+//此处是redux管理的全局状态的初始状态
+export const initialState={
+    isFetching: false,
+    hasLogin:false,
+    error:null,
+    user:{}
+};
