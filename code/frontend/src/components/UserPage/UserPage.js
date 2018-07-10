c6ebc256b58d141ec04fb5233356b6543cfae70a
princehaohao <1906974588@qq.com>
2018-07-10 15:29:45 +0800

    frontend shows charts

--- code/frontend/src/components/UserPage/UserPage.js
+++ code/frontend/src/components/UserPage/UserPage.js
@@ -3,17 +3,21 @@ import React, { Component,PropTypes } from 'react';
 import { Table, Icon, Switch, Radio, Form, Menu, Button, Tabs, Layout } from 'antd';
 import 'antd/dist/antd.css';
 
-import WrappedUserInfoPage from './UserInfoPage'
+import WrappedUserInfoPage from './UserInfoPage';
+import HeatmapPage from '../SensorImg/HeatmapPage'
+import LinechartPage from '../SensorImg/LinechartPage'
+
 
 const TabPane = Tabs.TabPane;
 const { Header,Content,Footer,Sider } = Layout;
 
-class UserChartPage extends Component{
+class UserHeatmapPage extends Component{
 
     render() {
         return(
+            
             <Layout>
-                <Content style={{ background: '#fff', padding: 0  }}>
+                
                 <Sider width={200} style={{ background: '#fff' , minHeight:900}}>
                     <Menu theme="light" mode="inline" defaultSelectedKeys={['1']} style={{textAlign:'left', minHeight: 500}}>
                         <Menu.Item key="1">
@@ -30,10 +34,45 @@ class UserChartPage extends Component{
                         </Menu.Item>
                     </Menu>
                 </Sider>
-                
-                        {/*此处放热力图*/}
+                <Content style={{ background: '#fff', padding: 0  }}>
+                    {/*此处放热力图*/}
+                    <HeatmapPage/>
                 
                 <Footer style={{ textAlign: 'center' }}>热力图</Footer>
+                </Content>
+            </Layout>
+        );
+    }
+}
+
+class UserLinechartPage extends Component{
+
+    render() {
+        return(
+            
+            <Layout>
+                
+                <Sider width={200} style={{ background: '#fff' , minHeight:900}}>
+                    <Menu theme="light" mode="inline" defaultSelectedKeys={['1']} style={{textAlign:'left', minHeight: 500}}>
+                        <Menu.Item key="1">
+                            <Icon type="shop" />
+                            <span className="nav-text">garden 1</span>
+                        </Menu.Item>
+                        <Menu.Item key="2">
+                            <Icon type="shop" />
+                            <span className="nav-text">garden 2</span>
+                        </Menu.Item>
+                        <Menu.Item key="3">
+                            <Icon type="shop" />
+                            <span className="nav-text">garden 3</span>
+                        </Menu.Item>
+                    </Menu>
+                </Sider>
+                <Content style={{ background: '#fff', padding: 0  }}>
+                    {/*此处放折线图*/}
+                    <LinechartPage/>
+                
+                <Footer style={{ textAlign: 'center' }}>折线图</Footer>
                 </Content>
             </Layout>
         );
@@ -45,8 +84,9 @@ class UserPage extends Component{
         return(
             <div>
                 <Tabs type={'card'}>
-                    <TabPane tab={'Thermodynamic Chart'} key={'1'}>{<UserChartPage/>}</TabPane>
-                    <TabPane tab={'User Information'} key={'2'}>{<WrappedUserInfoPage/>}</TabPane>
+                    <TabPane tab={'Thermodynamic Chart'} key={'1'}>{<UserHeatmapPage/>}</TabPane>
+                    <TabPane tab={'Line Chart'} key={'2'}>{<UserLinechartPage/>}</TabPane>
+                    <TabPane tab={'User Information'} key={'3'}>{<WrappedUserInfoPage/>}</TabPane>
                 </Tabs>
             </div>
         )
