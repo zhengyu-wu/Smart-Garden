31e1fc5aca3db16bc306fac5e2caaad1a8b67e80
GEORGE5961 <740223702@qq.com>
2018-07-10 09:18:10 +0800

    heatmap component finished

--- code/heatmap_component/src/App.js
+++ code/heatmap_component/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import data from './heatmap.json';
-import { Chart, Geom, Axis, Tooltip, Legend, Coord,Guide,Image } from 'bizcharts';
+import { Chart, Geom, Tooltip, Legend,Guide } from 'bizcharts';
 
-//const { Image } = Guide;
+const { Image } = Guide;
 
       
 class App extends Component {
+
 
     render(){
       return(
@@ -15,8 +16,7 @@ class App extends Component {
             <Tooltip showTitle={false}/>
             <Legend offset={10} />
             <Geom type='heatmap' position="g*l" color={['tmp', '#F51D27-#FA541C-#FF8C12-#FFC838-#FAFFA8-#80FF73-#12CCCC-#1890FF-#6E32C2']} />
-            <Guide >
-              <Image start={[ 'min', 'max' ]} end={[ 'max', 'min' ]} src='https://gw.alipayobjects.com/zos/rmsportal/NeUTMwKtPcPxIFNTWZOZ.png' />  
+            <Guide>
             </Guide>
           </Chart>
       );
