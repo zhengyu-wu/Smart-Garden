7da71050228e77ecea1018e7da95777c5b0a4817
GEORGE5961 <740223702@qq.com>
2018-07-10 17:30:37 +0800

    rearrange login component

--- code/ReactNative/demoWithRedux/src/components/Root.js
+++ code/ReactNative/demoWithRedux/src/components/Root.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import Login from './Login';
+import { Text, View , TextInput, ScrollView, StyleSheet} from 'react-native';
 
 import {connect} from 'react-redux';
 import UserPage from "./UserPage";
+
+const styles = StyleSheet.create({  
+    container: {    
+        flex: 1,    
+        top:100
+    }
+});
+
 
 class Root extends React.Component {
     render() {
         console.log("in Root "+JSON.stringify(this.props));
         if(this.props.user.hasLogin===true){
             return(
+
                 <UserPage/>
             );
         }
         else{
             return (
-                <Login/>
+                <View style={styles.container}>
+                    <Login />
+                </View>
+                
             );
         }
     }
