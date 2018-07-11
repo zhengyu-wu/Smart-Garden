10460e299ad093670faf89832841df2ae191a34a
NutterLee <Handong-Lee@sjtu.edu.cn>
2018-07-11 09:49:41 +0800

    更新到第一次迭代评审的版本

--- code/ReactNative/demoWithRedux/src/components/UserPage.js
+++ code/ReactNative/demoWithRedux/src/components/UserPage.js
@@ -3,7 +3,6 @@ import { Text, View ,TextInput,ScrollView} from 'react-native';
 import {connect} from 'react-redux';
 import {log_out} from "../actions";
 import Login from './Login'
-import {Button,List,InputItem} from 'antd-mobile-rn';
 import TabBars from './TabBar';
 
 class UserPage extends React.Component{
@@ -11,15 +10,8 @@ class UserPage extends React.Component{
         const {log_out}=this.props;
         if(this.props.user.hasLogin===true)
             return(
-            <View>
-                <ScrollView>
-                    <Button type={'primary'} onClick={()=>log_out()}>
-                        Logout
-                    </Button>
-                </ScrollView>
-                <View style={{height:45}}>
-                    <TabBars/>
-                </View>
+            <View style={{flex:1}}>
+                <TabBars/>
             </View>);
         else return(
             <View>
