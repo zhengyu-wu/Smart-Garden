b640310253f839967a915a0ea9dd320c69cae03c
NutterLee <Handong-Lee@sjtu.edu.cn>
2018-07-12 10:34:16 +0800

    完成注册界面和对应跳转
    存在部分问题，问题如下：
    1. flex布局有一些问题
    2. 点击注册按钮就会触发跳转

--- code/ReactNative/demoWithRedux/src/reducers/user.js
+++ code/ReactNative/demoWithRedux/src/reducers/user.js
@@ -1,4 +1,11 @@
-import {LOAD_USER_FULFILLED, LOAD_USER_PENDING, LOAD_USER_REJECTED, USER_LOG_OUT} from '../constants';
+import {
+    CLEAR_REGISTER,
+    LOAD_USER_FULFILLED,
+    LOAD_USER_PENDING,
+    LOAD_USER_REJECTED,
+    REGISTER_FULFILLED, REGISTER_PENDING, REGISTER_REJECTED,
+    USER_LOG_OUT
+} from '../constants';
 import {Toast} from 'antd-mobile-rn';
 import {initialState} from '../state';
 
@@ -48,6 +55,29 @@ const user = (state = initialState, action={}) => {
                 hasLogin:false,
                 user: {}
             };
+        case REGISTER_FULFILLED:
+            Toast.success("register successfully",1);
+            return {
+                ...state,
+                hasRegister:true
+            };
+        case REGISTER_PENDING:
+            Toast.info("processing",1);
+            return {
+                ...state,
+                hasRegister:false
+            };
+        case REGISTER_REJECTED:
+            Toast.info("register fails",1);
+            return {
+                ...state,
+                hasRegister:false
+            };
+        case CLEAR_REGISTER:
+            return {
+                ...state,
+                hasRegister:false
+            }
         default:
             return state;
     }
