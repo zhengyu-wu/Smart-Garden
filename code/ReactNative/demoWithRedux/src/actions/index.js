a8be7aefbada4345d5ec66be94fa1eba39b448c7
NutterLee <Handong-Lee@sjtu.edu.cn>
2018-07-13 08:53:05 +0800

    完成修改个人信息界面与相应跳转
    待完善：css与显示的形式

--- code/ReactNative/demoWithRedux/src/actions/index.js
+++ code/ReactNative/demoWithRedux/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {LOAD_USER, REGISTER, REGISTER_REJECTED, USER_LOG_OUT,CLEAR_REGISTER} from '../constants';
+import {LOAD_USER, USER_LOG_OUT} from '../constants';
 
 
 export const get_user = (email,password) => {
@@ -14,36 +14,5 @@ export const get_user = (email,password) => {
 export const log_out =()=>{
     return{
         type: USER_LOG_OUT
-    }
-};
-
-export const register = (username,phone,email,password,navigation) => {
-    if(username===""||phone===""||email===""||password==="")
-        return{
-            type: REGISTER_REJECTED,
-        };
-    else{
-        return {
-            type: REGISTER,
-            payload: {
-                promise: axios.post("http://192.168.56.1:8080/users/addUser",{
-                    'username': username,
-                    'phone': phone,
-                    'email': email,
-                    'password': password
-                },{
-                    "headers": {
-                        'Content-Type': 'application/json',
-                    }
-                })
-            }
-        };
-    }
-
-};
-
-export const clear_register =()=>{
-    return{
-        type:CLEAR_REGISTER,
     }
 };
